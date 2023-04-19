11d4287cbc061b399a90f99ccbbb699547457f60
Spencer Kendall <spencerlkendall@gmail.com>
2023-04-19 14:44:44 -0500

    getMovies working, images and titles not loading

--- public/index.js
+++ public/index.js
@@ -1,62 +1,58 @@
+let baseURL = `http://localhost:5000/api/movies`
+
 let moviesForm = document.getElementById('movies-form')
-let listForm = document.getElementById('list-form')
 let movieName = document.getElementById('movie-name')
 let movieURL = document.getElementById('movie-url')
-let listName = document.getElementById('list-name')
 let moviesDisplay = document.getElementById('movies-display')
-let loadingReel = document.getElementById('loading-reel')
 
-moviesForm.addEventListener('submit', (event) => {
-    event.preventDefault()
+let moviesCallBack = ({data: movies}) => displayMovies(movies)
+let errCallBack = err => console.log(err.response.data)
 
-    if (
-        movieName.value === '' ||
-        movieURL.value === ''
-    ) {
-        alert ('Please fill out all inputs for your movie.')
-        return
+let getMovies = () => axios.get(baseURL)
+    .then(moviesCallBack)
+    .catch(errCallBack)
+
+let createMovie = body => axios.post(baseURL, body)
+    .then(moviesCallBack)
+    .catch(errCallBack)
+
+    moviesForm.addEventListener('submit', (event) => {
+            event.preventDefault()
+        
+            if (
+                movieName.value === '' ||
+                movieURL.value === ''
+            ) {
+                alert ('Please fill out all inputs for your movie.')
+                return
+            }
+            
+            let bodyObj = {
+                movieName: movieName.value,
+                movieURL: movieURL.value
+            }
+
+            createMovie(bodyObj)
+
+            movieName.value = ''
+            movieURL.value = ''
+
+        })
+
+function createMovieWrapper(movie) {
+    let movieWrapper = document.createElement('div')
+    movieWrapper.classList.add('movie-wrapper')
+    movieWrapper.innerHTML = `<img alt='movie-poster' src=${movie.movieURL} class='movie-poster'/>
+    <p class="movie-title>${movie.movieName}</p>
+    `
+    moviesDisplay.appendChild(movieWrapper)
+}
+
+function displayMovies(arr) {
+    moviesDisplay.innerHTML = ``
+    for (let i = 0; i < arr.length; i++) {
+        createMovieWrapper(arr[i])
     }
-    
-    let bodyObj = {
-        name: movieName.value,
-        poster: movieURL.value
-    }
+}
 
-    axios.post('http://localhost:5000/movies', bodyObj)
-        .then((result) => {getMoviesDropDown()}
-        )
-
-    movieName.value = ''
-    movieURL.value = ''
-})
-
-listForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    if (
-        listName.value ===''
-    ) {
-        alert('Please assign a name to your list.')
-        return
-    }
-
-    moviesDisplay.innerHTML = ''
-    loadingReel.classList.remove('hidden')
-
-    let bodyObj = {
-        name: listName.value
-    }
-
-    axios.post('http://localhost:5000/lists', bodyObj)
-    .then((result) => {updateData()}
-    ).catch(() => {
-        loadingReel.classList.add('hidden')
-        moviesDisplay.innerHTML = ''
-        alert('Error within data.')
-        return
-    })
-
-    listName.value = ''
-
-})
-
+getMovies()
--- public/lists.js
+++ public/lists.js
@@ -1,2 +1,66 @@
 //utilize the index.js page from f37-5-review-main
 //particularly the getfighterlist for getMoviesDropDown and so on
+
+
+// let moviesForm = document.getElementById('movies-form')
+// let listForm = document.getElementById('list-form')
+// let movieName = document.getElementById('movie-name')
+// let movieURL = document.getElementById('movie-url')
+// let listName = document.getElementById('list-name')
+// let moviesDisplay = document.getElementById('movies-display')
+// let loadingReel = document.getElementById('loading-reel')
+
+// moviesForm.addEventListener('submit', (event) => {
+//     event.preventDefault()
+
+//     if (
+//         movieName.value === '' ||
+//         movieURL.value === ''
+//     ) {
+//         alert ('Please fill out all inputs for your movie.')
+//         return
+//     }
+    
+//     let bodyObj = {
+//         name: movieName.value,
+//         poster: movieURL.value
+//     }
+
+//     axios.post('http://localhost:5000/movies', bodyObj)
+//         .then((result) => {getMoviesDropDown()}
+//         )
+
+//     movieName.value = ''
+//     movieURL.value = ''
+// })
+
+// listForm.addEventListener('submit', (event) => {
+//     event.preventDefault()
+
+//     if (
+//         listName.value ===''
+//     ) {
+//         alert('Please assign a name to your list.')
+//         return
+//     }
+
+//     moviesDisplay.innerHTML = ''
+//     loadingReel.classList.remove('hidden')
+
+//     let bodyObj = {
+//         name: listName.value
+//     }
+
+//     axios.post('http://localhost:5000/lists', bodyObj)
+//     .then((result) => {updateData()}
+//     ).catch(() => {
+//         loadingReel.classList.add('hidden')
+//         moviesDisplay.innerHTML = ''
+//         alert('Error within data.')
+//         return
+//     })
+
+//     listName.value = ''
+
+// })
+
