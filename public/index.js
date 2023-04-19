e0ab11df39a6bb502b193c2ec7d56b1818e4fe3b
Spencer Kendall <spencerlkendall@gmail.com>
2023-04-19 01:44:37 -0500

    Event listeners for empty input fields added

--- public/index.js
+++ public/index.js
@@ -6,4 +6,56 @@ let listName = document.getElementById('list-name')
 let moviesDisplay = document.getElementById('movies-display')
 let loadingReel = document.getElementById('loading-reel')
 
-moviesForm.addEventListener('submit')
+moviesForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (
+        movieName.value === '' ||
+        movieURL.value === ''
+    ) {
+        alert ('Please fill out all inputs for your movie.')
+        return
+    }
+    
+    let bodyObj = {
+        name: movieName.value,
+        poster: movieURL.value
+    }
+
+    axios.post('http://localhost:5000/movies', bodyObj)
+        .then((result) => {getMoviesList()}
+        )
+
+    movieName.value = ''
+    movieURL.value = ''
+})
+
+listForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (
+        listName.value ===''
+    ) {
+        alert('Please assign a name to your list.')
+        return
+    }
+
+    moviesDisplay.innerHTML = ''
+    loadingReel.classList.remove('hidden')
+
+    let bodyObj = {
+        name: listName.value
+    }
+
+    axios.post('http://localhost:5000/lists', bodyObj)
+    .then((result) => {updateData()}
+    ).catch(() => {
+        loadingReel.classList.add('hidden')
+        moviesDisplay.innerHTML = ''
+        alert('Error within data.')
+        return
+    })
+
+    listName.value = ''
+
+})
